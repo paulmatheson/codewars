7c3675fd84ac135c01c4548f0dd48ff940b99804
paulmatheson <iampaulmatheson@gmail.com>
2018-09-19 20:32:25 -0700

    Exercise 02 complete

---
+++ build-tower.js
@@ -0,0 +1,29 @@
+//CodeWars: Build Tower Exercise
+
+//Build Tower by the following given argument:
+//number of floors (integer and always greater than 0).
+
+//Tower block is represented as *
+
+//code:
+
+function towerBuilder(nFloors) {
+  var tower = [];
+  
+  for (var i = 1; i <= nFloors; i++) {
+    tower.push("");
+    
+    for (var k = 0; k < (nFloors - i); k++) {
+      tower[i-1] += " ";
+    }
+    
+    for (var k = 0; k < ((i * 2) - 1); k++) {
+      tower[i-1] += "*";
+    }
+    
+    for (var k = 0; k < (nFloors - i); k++) {
+      tower[i-1] += " ";
+    }
+  }
+  return tower;
+}
