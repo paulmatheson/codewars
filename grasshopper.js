c12850b039f4c8578c01ec2859a21724a3ea90ae
paulmatheson <iampaulmatheson@gmail.com>
2022-02-27 10:36:39 -0800

    Added new projects

---
+++ grasshopper.js
@@ -0,0 +1,12 @@
+/*
+Instructions:
+Create a function that gives a personalized greeting. This function takes two parameters: name and owner.
+
+Use conditionals to return the proper message:
+name equals owner -	'Hello boss'
+otherwise -	'Hello guest'
+*/
+
+function greet (name, owner) {
+  return (name == owner) ? "Hello boss" : "Hello guest"
+}
