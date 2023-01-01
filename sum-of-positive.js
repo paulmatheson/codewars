58f70e825b00c2a550b0071224956d4af58f3931
Paul <iampaulmatheson@gmail.com>
2023-01-01 14:39:35 -0500

    Added sum of positive

---
+++ sum-of-positive.js
@@ -0,0 +1,14 @@
+//Title : Sum of positive
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+function positiveSum(arr) {
+    return arr.reduce((acc, currVal) => {
+        return acc + (currVal > 0 ? currVal : 0)
+    }, 0)
+}
