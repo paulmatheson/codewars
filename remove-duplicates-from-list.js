41e91fcc0a924e1279a6de14cfaca01f2f344acb
Paul Matheson <paul.a.matheson@avanade.com>
2024-05-14 16:55:19 -0700

    New solution.

--- remove-duplicates-from-list.js
+++ remove-duplicates-from-list.js
@@ -1,3 +1,9 @@
+/* Code Wars: Remove duplicates from list
+Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+The order of the sequence has to stay the same.
+*/
+
 function distinct(a) {
     let uniqueArr = [] // initialize new arr 
     for (let i = 0; i < a.length; i++) { // iterate through passed arr and check if new arr contains
@@ -6,4 +12,4 @@ function distinct(a) {
       }
     }
     return uniqueArr;
-  }
+}
